9dfed11c10379588028d8852a5340e1a28591baf
KovacsSebestyen <kovacsseb2002@gmail.com>
2023-11-28 17:31:01 +0100

    Validator changes

--- szkd/src/main/java/hu/nye/kovacssebestyen/szkd/tarszki/service/Validator.java
+++ szkd/src/main/java/hu/nye/kovacssebestyen/szkd/tarszki/service/Validator.java
@@ -4,49 +4,242 @@ public class Validator {
 
     //
 
-    public Boolean check(String formula){
+    public String check(String formula){
+        if (formula.equals("(true)")) {
+            return "(true)";
+        }
+        if (formula.equals("(false)")) {
+           return "(false)";
+        }
+
         char[] formulaChar = formula.toCharArray();
 
-        for (int i=0; i < formulaChar.length; i++){
-
-            if(formulaChar[i] == '&'){
-                int depth = 0;
-                int index = 0;
-                for(int j=i+1; j<formulaChar.length; j++){
-                    if(formulaChar[j] == '('){
-                        depth++;
-                    }
-                    if(formulaChar[j] == ')'){
-                        depth--;
-                    }
-                    if(depth == 0){
-                        index = j;
-                        j = formulaChar.length+1;
-                    }
+        //fölösleges zárójelek eltüntetése
+        if (formulaChar[0] == '(' && formulaChar[formulaChar.length-1] == ')') {
+            int depth = 0;
+            boolean useless = true;
+            for (int i = 0; i < formulaChar.length; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
                 }
-                String subFormula1 = formula.substring(i + 1, index);
-                for(int j=i-1; j>=0; j--){
-                    if(formulaChar[j] == ')'){
-                        depth++;
-                    }
-                    if(formulaChar[j] == '('){
-                        depth--;
-                    }
-                    if(depth == 0){
-                        index = j;
-                        j = -1;
-                    }
+                if (formulaChar[i] == ')') {
+                    depth--;
                 }
-                String subFormula2 = formula.substring(index, i - 1);
-                //for ciklus ugorjon a kifejezés után
-                return check(subFormula1) && check(subFormula2);
+                if (depth == 0 && i != formulaChar.length - 1) {
+                    i = formulaChar.length + 1;
+                    useless = false;
+                }
+            }
+            if (useless) {
+                return check(formula.substring(1, formulaChar.length-1));       // (d(s) V s(f) V a(f)) --> d(s) V s(f) V a(f)
             }
         }
-        return true;
+
+        //'<=>' keresése
+        if (formula.contains("<=>")) {
+            int depth = 0;
+            int index = 0;
+            boolean foundAtZeroDepth = false;
+            for (int i = 0; i < formulaChar.length-2; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
+                }
+                if (formulaChar[i] == ')') {
+                    depth--;
+                }
+                if(formulaChar[i] == '<' && formulaChar[i] == '=' && formulaChar[i + 1] == '>' && depth == 0) {
+                    index = i;
+                    foundAtZeroDepth = true;
+                    i = formulaChar.length;
+                }
+            }
+            if (foundAtZeroDepth) {
+                boolean left = false;
+                boolean right = false;
+                if (check(formula.substring(0, index)).equals("(true)")) {
+                    left = true;
+                }
+                if (check(formula.substring(index + 3)) == "(true)") {
+                    right = true;
+                }
+                if (left && right) {
+                    return "(true)";
+                }
+                if (left && !right) {
+                    return "(false)";
+                }
+                if (!left && right) {
+                    return "(false)";
+                }
+                if (!left && !right) {
+                    return "(true)";
+                }
+            }
+        }
+
+        //'=>' keresése
+        if (formula.contains("=>")) {
+            int depth = 0;
+            int index = 0;
+            boolean foundAtZeroDepth = false;
+            for (int i = 0; i < formulaChar.length-1; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
+                }
+                if (formulaChar[i] == ')') {
+                    depth--;
+                }
+                if(formulaChar[i] == '=' && formulaChar[i + 1] == '>' && depth == 0) {
+                    index = i;
+                    foundAtZeroDepth = true;
+                    i = formulaChar.length;
+                }
+            }
+            if (foundAtZeroDepth) {
+                boolean left = false;
+                boolean right = false;
+                if (check(formula.substring(0, index)).equals("(true)")) {
+                    left = true;
+                }
+                if (check(formula.substring(index + 2)) == "(true)") {
+                    right = true;
+                }
+                if (left && right) {
+                    return "(true)";
+                }
+                if (left && !right) {
+                    return "(false)";
+                }
+                if (!left && right) {
+                    return "(true)";
+                }
+                if (!left && !right) {
+                    return "(true)";
+                }
+            }
+        }
+
+        //'\/' keresése
+        if (formula.contains("\\/")) {
+            int depth = 0;
+            int index = 0;
+            boolean foundAtZeroDepth = false;
+            for (int i = 0; i < formulaChar.length-1; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
+                }
+                if (formulaChar[i] == ')') {
+                    depth--;
+                }
+                if(formulaChar[i] == '\\' && formulaChar[i + 1] == '/' && depth == 0) {
+                    index = i;
+                    foundAtZeroDepth = true;
+                    i = formulaChar.length;
+                }
+            }
+            if (foundAtZeroDepth) {
+                boolean left = false;
+                boolean right = false;
+                if (check(formula.substring(0, index)).equals("(true)")) {
+                    left = true;
+                }
+                if (check(formula.substring(index + 2)) == "(true)") {
+                    right = true;
+                }
+                if (left && right) {
+                    return "(true)";
+                }
+                if (left && !right) {
+                    return "(true)";
+                }
+                if (!left && right) {
+                    return "(true)";
+                }
+                if (!left && !right) {
+                    return "(false)";
+                }
+            }
+        }
+
+        //'/\' keresése
+        if (formula.contains("/\\")) {
+            int depth = 0;
+            int index = 0;
+            boolean foundAtZeroDepth = false;
+            for (int i = 0; i < formulaChar.length-1; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
+                }
+                if (formulaChar[i] == ')') {
+                    depth--;
+                }
+                if(formulaChar[i] == '/' && formulaChar[i + 1] == '\\' && depth == 0) {
+                    index = i;
+                    foundAtZeroDepth = true;
+                    i = formulaChar.length;
+                }
+            }
+            if (foundAtZeroDepth) {
+                boolean left = false;
+                boolean right = false;
+                if (check(formula.substring(0, index)).equals("(true)")) {
+                    left = true;
+                }
+                if (check(formula.substring(index + 2)) == "(true)") {
+                    right = true;
+                }
+                if (left && right) {
+                    return "(true)";
+                }
+                if (left && !right) {
+                    return "(false)";
+                }
+                if (!left && right) {
+                    return "(false)";
+                }
+                if (!left && !right) {
+                    return "(false)";
+                }
+            }
+        }
+
+        //'-' keresése
+        if(formula.contains("-")) {
+            int depth = 0;
+            int index = 0;
+            boolean foundAtZeroDepth = false;
+            for (int i = 0; i < formulaChar.length; i++) {
+                if (formulaChar[i] == '(') {
+                    depth++;
+                }
+                if (formulaChar[i] == ')') {
+                    depth--;
+                }
+                if(formulaChar[i] == '-' && depth == 0) {
+                    index = i;
+                    foundAtZeroDepth = true;
+                    i = formulaChar.length;
+                }
+            }
+            if (foundAtZeroDepth) {
+                boolean negation = false;
+                if (check(formula.substring(index + 1)).equals("(true)")) {
+                    negation = true;
+                }
+                if (!negation) {
+                    return "(true)";
+                }
+                if (negation) {
+                    return "(false)";
+                }
+            }
+        }
+
+        return "";
     }
 
-    public Boolean small(Character shapeName){
+    public String small(Character shapeName){
 
-        return true;
+        return "(true)";
     }
 }
